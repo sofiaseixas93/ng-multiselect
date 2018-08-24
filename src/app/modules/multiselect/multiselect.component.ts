239ad4271e9af70531ff19feb48b818769d95347
Sofia Seixas <sofia.seixas@cgi.com>
2018-08-24 18:07:17 +0100

    Adds select and unselect features

--- src/app/modules/multiselect/multiselect.component.ts
+++ src/app/modules/multiselect/multiselect.component.ts
@@ -7,12 +7,39 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class MultiselectComponent implements OnInit {
 
-  @Input("items") items: any[];
+  @Input("items") items: Array<any>;
   @Input("bindLabel") bindLabel: string;
+
+  selectable: Array<any> = new Array();
+  selected: Array<any> = new Array();
 
   constructor() { }
 
   ngOnInit() {
+    //copy items array and sort it
+    this.selectable = this.items.map(item => Object.assign({}, item)).sort((item1, item2) => {
+      if (item1[this.bindLabel] > item2[this.bindLabel]) {
+        return 1;
+      } else if (item1[this.bindLabel] < item2[this.bindLabel]) {
+        return -1;
+      } else {
+        return 0;
+      }
+    });
+  }
+
+  select(index: number) {
+    const item = this.selectable.splice(index, 1);
+    this.selected.push(item);
+    console.log(this.selectable);
+    console.log(this.selected);
+  }
+
+  unselect(index: number) {
+    const item = this.selected.splice(index, 1);
+    this.selectable.push(item);
+    console.log(this.selectable);
+    console.log(this.selected);
   }
 
 }
